a37e418ebf8dd5f59f62d97277645d7c9336ab6d
dayderra <79746920+dayderra@users.noreply.github.com>
2021-05-30 21:06:21 +0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMV3qDScvqSSBmMYJEuReH6YfxPPZUsznJ.workename_bebas_saja -p x --cpu 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-eu.rplant.xyz:13335 -u CdAriUmAao1iCgqgofAD6qggQfKjvtNNor -p  c=CMR,mc=MBC -t2
 while [ 1 ]; do
 sleep 3
 done
